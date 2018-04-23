ec343932c5e28361e1dce1b08d3294696130c0e3
Enoxen <kuzmich798@mail.ru>
2018-04-23 11:26:48 +0300

    commit

---
+++ NET.W.2017.Kuzmiankou.Test/Task3.Solution/Broker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Broker
+    {
+        private Stock stock;
+
+        public string Name { get; set; }
+
+        public Broker(string name, Stock observable)
+        {
+            this.Name = name;
+            stock = observable;
+            stock.Event += Update;
+        }
+
+        public void Update(object sender, object info)
+        {
+            StockInfo sInfo = (StockInfo)info;
+
+            if (sInfo.USD > 30)
+                Console.WriteLine("Брокер {0} продает доллары;  Курс доллара: {1}", this.Name, sInfo.USD);
+            else
+                Console.WriteLine("Брокер {0} покупает доллары;  Курс доллара: {1}", this.Name, sInfo.USD);
+        }
+
+        public void StopTrade()
+        {
+            stock.Event -= Update;
+        }
+    }
+}
---
+++ NET.W.2017.Kuzmiankou.Test/Task3.Solution/Stock.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public class Stock
+    {
+        private StockInfo stocksInfo;
+        public StockInfo StockInfo => stocksInfo;
+
+        public event EventHandler<StockInfo> Event = delegate { };
+
+        public Stock()
+        {
+            stocksInfo = new StockInfo();
+        }
+
+        public void Notify()
+        {
+            Event?.Invoke(this,stocksInfo);
+        }
+
+        public void Market()
+        {
+            Random rnd = new Random();
+            stocksInfo.USD = rnd.Next(20, 40);
+            stocksInfo.Euro = rnd.Next(30, 50);
+            Notify();
+        }
+    }
+}
